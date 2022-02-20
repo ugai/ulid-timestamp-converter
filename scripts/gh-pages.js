34d84ddf7f88c6bc470624924a20f1ac306f4ab4
ugai <ugai.sub1@gmail.com>
2022-02-20 23:24:23 +0900

    Add gh-pages deploying script

---
+++ scripts/gh-pages.js
@@ -0,0 +1,17 @@
+var ghpages = require('gh-pages');
+
+ghpages.publish(
+    'public',
+    {
+        branch: 'gh-pages',
+        silent: true,
+        repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/ugai/ulid-datetime-converter.git',
+        user: {
+            name: 'ugai',
+            email: 'ugai.sub1@gmail.com'
+        }
+    },
+    () => {
+        console.log('Deploy Complete!')
+    }
+)
